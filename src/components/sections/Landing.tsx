ef6f3c9fb3a2ede2fbb81b6218ab3b692317f01b
Manas Mahanand <manasmahanand@proton.me>
2024-11-23 16:22:58 +0530

    centered text and nav in landing

--- src/components/sections/Landing.tsx
+++ src/components/sections/Landing.tsx
@@ -1,40 +1,6 @@
-<<<<<<< HEAD
-import { createSignal, For, onMount } from "solid-js";
-import Nav from "../Nav";
-import anime from 'animejs/lib/anime.es.js';
-
-export default function Landing() {
-	const [isAnimating, setIsAnimating] = createSignal(false);
-
-	const text = "More Tech.";
-	let highlight: HTMLSpanElement[] = [];
-
-	for (let i = 0; i < text.length; i++) {
-		highlight.push(<span class="inline-block">{text.charAt(i)}</span> as HTMLSpanElement);
-	}
-
-	const bounceLetters = (index = 0) => {
-		console.log('called heer!!')
-		if ((index === 0 && !isAnimating) || index < highlight.length) {
-			setIsAnimating(true);
-			setTimeout(() => {
-				anime({
-					targets: highlight[index],
-					translateY: [0, -5, 0],
-					duration: 800
-				});
-				index++;
-				bounceLetters(index);
-			}, 100);
-		} else {
-			setIsAnimating(false);
-		}
-	};
-=======
 import Nav from "../Nav";
 
 export default function Landing() {
->>>>>>> 3628afa (removed hover effect)
 
 	return (
 		<div>
@@ -45,19 +11,10 @@ export default function Landing() {
 						<h1 class="mb-4">I'm Keagan</h1>
 						<h1 class="italic flex items-center">
 							Less Talk,
-<<<<<<< HEAD
-							<span class="flex flex-col">
-								<span class="text-primary mx-2 cursor-pointer" onMouseEnter={() => bounceLetters()}>
-									{highlight}
-								</span>
-								<div class="h-1 w-[95%] mx-auto bg-primary"></div>
-							</span>
-=======
-							<span class="text-primary mx-2 cursor-pointer underline">More Tech!</span>
->>>>>>> 3628afa (removed hover effect)
+							<span class="text-primary mx-2 cursor-pointer underline hover:text-secondary transition-colors">More Tech!</span>
 						</h1>
 					</div>
-					<div class="relative top-40 scale-125 h-7 w-4 rounded-full border-primary border-2 ml-1 flex justify-center py-2">
+					<div class="relative top-40 scale-125 h-7 w-4 rounded-full border-primary border-2 ml-1 flex justify-center py-2 cursor-pointer hover:scale-[140%] transition-transform">
 						<div class="h-1 w-1 rounded-full bg-primary animate-bounce"></div>
 					</div>
 				</header>
@@ -65,4 +22,3 @@ export default function Landing() {
 		</div>
 	)
 }
-
