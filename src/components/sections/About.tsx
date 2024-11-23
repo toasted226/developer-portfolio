e9bbdf7942141b675bf5d01958323d5304148de1
Keagan Collins <keaganncollins@gmail.com>
2024-11-23 12:50:37 +0200

    added footer icons

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -22,11 +22,11 @@ export default function About(props: {class?: string}) {
 	});
 
 	return (
-		<div class={`pb-40 ${props.class}`}>
+		<div class={`pb-52 ${props.class}`}>
 			<section class="container px-10">
 				<h1 class="text-center text-2xl py-16">about</h1>
 				<div class="grid grid-cols-2">
-					<div class="h-20 border-r-white border-solid border-r-[1px] text-sm flex justify-center items-center">
+					<div class="h-full border-r-white border-solid border-r-[1px] text-sm flex justify-center items-center">
 						<p class="w-96">Lorem ipsum odor amet, consectetuer adipiscing elit. Vehicula sit porta primis penatibus risus sed, semper scelerisque odio. Mauris taciti primis torquent porttitor tincidunt a.</p>
 					</div>
 					<div class="relative" ref={(el) => (containerRef = el)}>
@@ -88,7 +88,6 @@ const Hobby: Component<{svg: Component<ComponentProps<"svg">>, size?: string, na
 
 		const direction: Position = {x: localMousePos.x - origin().x, y: localMousePos.y - origin().y};
 		const distance = Math.sqrt(direction.x ** 2 + direction.y ** 2);
-		console.log(`DIV-${props.id} distance: ${distance}`);
 
 		const limitedDistance = Math.min(distance, MAX_DISTANCE);
 
--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,9 +1,15 @@
+import Email from "../icons/Email";
+import Github from "../icons/Github"
+import Linkedin from "../icons/Linkedin"
+
 export default function Footer() {
 	return (
-		<div class="w-full h-36 bg-secondary-background flex justify-center items-center gap-8">
-			<a>GitHub</a>
-			<a>LinkedIn</a>
-			<a>Email</a>
+		<div class="w-full h-36 bg-secondary-background flex items-center justify-center">
+			<div class="h-8 flex justify-center items-center gap-16">
+				<Github class="h-full hover:scale-110 transition-transform cursor-pointer" />
+				<Linkedin class="h-full hover:scale-110 transition-transform cursor-pointer" />
+				<Email class="h-full hover:scale-110 transition-transform cursor-pointer" />
+			</div>
 		</div>
 	);
 }
