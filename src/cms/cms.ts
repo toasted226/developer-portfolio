647e62e5950a3da02e798e75c70d243168e4a2ac
Keagan <keaganncollins@gmail.com>
2026-03-09 14:07:42 +0200

    added source to list of skills

--- src/cms/cms.ts
+++ src/cms/cms.ts
@@ -1,50 +1,56 @@
 export type Skill = string;
 
 export interface Project {
-	name: string,
-	desc: string,
-	url: string,
-	tags: Skill[],
+  name: string,
+  desc: string,
+  url: string,
+  tags: Skill[],
 }
 
 export const projects: Project[] = [
-	{
-		name: "Colourful Assembly Language",
-		desc: "Esoteric assembly-like language written as colours.",
-		url: "https://github.com/toasted226/colourful-assembly-language",
-		tags: ["C#"]
-	},
-	{
-		name: "Conlang Dictionary",
-		desc: "A Constructed Language Dictionary built for my friends.",
-		url: "https://github.com/toasted226/conlang-dictionary",
-		tags: ["TS", "JS", "Go", "Angular"]
-	},
-	{
-		name: "One-Handed Keyboard",
-		desc: "Small notepad allowing users to touch-type with one hand.",
-		url: "https://github.com/toasted226/onehandkeyboard",
-		tags: ["Rust", "TS", "JS", "React"]
-	},
-	{
-		name: "Downloads Sorter",
-		desc: "Tiny automation program to organise your downloads folder.",
-		url: "https://github.com/toasted226/downloads-sorter",
-		tags: ["Rust"]
-	},
-	{
-		name: "Office Chaos",
-		desc: "Game built in 7 days for Brackey's Game Jam 2021.",
-		url: "https://github.com/toasted226/brackeys--gamejam-2021.2",
-		tags: ["C#"]
-	},
-	{
-		name: "Developer Portfolio",
-		desc: "You're looking at it!",
-		url: "https://github.com/toasted226/developer-portfolio",
-		tags: ["TS", "JS", "Solid"]
-	}
+  {
+    name: "Source Backend",
+    desc: "Clojure-written backend for the Source platform.",
+    url: "https://github.com/modulr-software/source-be",
+    tags: ["Clojure"]
+  },
+  {
+    name: "Colourful Assembly Language",
+    desc: "Esoteric assembly-like language written as colours.",
+    url: "https://github.com/toasted226/colourful-assembly-language",
+    tags: ["C#"]
+  },
+  {
+    name: "Conlang Dictionary",
+    desc: "A Constructed Language Dictionary built for my friends.",
+    url: "https://github.com/toasted226/conlang-dictionary",
+    tags: ["TS", "JS", "Go", "Angular"]
+  },
+  {
+    name: "One-Handed Keyboard",
+    desc: "Small notepad allowing users to touch-type with one hand.",
+    url: "https://github.com/toasted226/onehandkeyboard",
+    tags: ["Rust", "TS", "JS", "React"]
+  },
+  {
+    name: "Downloads Sorter",
+    desc: "Tiny automation program to organise your downloads folder.",
+    url: "https://github.com/toasted226/downloads-sorter",
+    tags: ["Rust"]
+  },
+  {
+    name: "Office Chaos",
+    desc: "Game built in 7 days for Brackey's Game Jam 2021.",
+    url: "https://github.com/toasted226/brackeys--gamejam-2021.2",
+    tags: ["C#"]
+  },
+  {
+    name: "Developer Portfolio",
+    desc: "You're looking at it!",
+    url: "https://github.com/toasted226/developer-portfolio",
+    tags: ["TS", "JS", "Solid"]
+  }
 ];
 
-export const skills: Skill[] = ["Rust", "Go", "JS", "C#"];
+export const skills: Skill[] = ["Rust", "Go", "JS", "C#", "Clojure"];
 
